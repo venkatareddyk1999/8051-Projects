aaaac395df6fecde691bf936d0a866d71463685a
VENKATA REDDY KOPPARTHI <you@kvenkatareddy10@gmail.com>
2023-11-19 14:56:24 +0530

    Only C file is there

--- RELAY/main2.d
+++
@@ -1,7 +0,0 @@
-.\main2.o: main2.c
-.\main2.o: C:\Keil\ARM\Inc\NXP\LPC17xx\lpc17xx.h
-.\main2.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
-.\main2.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\main2.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
-.\main2.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
-.\main2.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h

2b74376210e44868d039babcbe6abba6cb43e8b6
VENKATA REDDY KOPPARTHI <you@kvenkatareddy10@gmail.com>
2023-11-19 14:48:30 +0530

    Added my project relay another relay

---
+++ RELAY/RELAY/main1.d
@@ -0,0 +1,7 @@
+.\main1.o: main1.c
+.\main1.o: C:\Keil\ARM\Inc\NXP\LPC17xx\lpc17xx.h
+.\main1.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+.\main1.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\main1.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\main1.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\main1.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
